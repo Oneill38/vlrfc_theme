b0b784b47121a78c20ce6fbe35411cf66eec153b
Megan O'Neill <mo2638@columbia.edu>
2015-03-03 20:25:49 -0500

    vs image fix

--- single-boxing.php
+++ single-boxing.php
@@ -32,7 +32,7 @@ get_header(); ?>
 	      			
 	      			<h1 class="fight_title">
 	      				<div class="boxer_name_title"><?php echo $name ?></div>
-	      				<img id="versus" src="wp-content/themes/vlrfc_theme/images/versus.png">
+	      				<img id ="versus" src="<?php bloginfo('template_directory'); ?>/images/versus.png" alt="versus" />
 	      				<div class="boxer_name_title"><?php echo $name_two ?></div>
 	      			</h1>
 
